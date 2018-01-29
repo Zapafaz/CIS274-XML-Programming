ca8f848debd0bfd6ae9a98d5d0f70ddab0028f45
Zapafaz <Zapafaz@gmail.com>
2018-01-28 22:17:06 -0500

    initial commit

---
+++ Weekly Homework/Code/Week2/CIS274_XML_Programming_Project/CIS274_XML_Programming_Project/PoemConverter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CIS274_XML_Programming_Project
+{
+    public class PoemConverter
+    {
+        public string SourceFolderPath { get; }
+        public string TargetFilePath { get; }
+        public XmlDocument PoemSet { get; }
+
+        public PoemConverter(string folderPath, string targetFilePath)
+        {
+            SourceFolderPath = folderPath;
+            TargetFilePath = targetFilePath;
+            PoemSet = new XmlDocument();
+        }
+
+        public string[] FileTextToLines(string path)
+        {
+            var lines = new List<string>();
+            using (var reader = new StreamReader(path))
+            {
+                while(reader.Peek() > -1)
+                {
+                    lines.Add(reader.ReadLine());
+                }
+            }
+            return lines.ToArray();
+        }
+
+        public void LinesToXml(string[] allLines)
+        {
+            bool startNewStanza = true;
+            using (var writer = new StreamWriter(TargetFilePath, true))
+            {
+                writer.WriteLine("\t<Poem>");
+                foreach(string line in allLines)
+                {
+                    if (startNewStanza)
+                    {
+                        writer.WriteLine("\t\t<Stanza>");
+                        startNewStanza = false;
+                    }
+                    if (line.Length > 0)
+                    {
+                        writer.WriteLine($"\t\t\t<PoemLine>{line}</PoemLine>");
+                    }
+                    else
+                    {
+                        writer.WriteLine("\t\t</Stanza>");
+                        startNewStanza = true;
+                    }
+                }
+                writer.WriteLine("\t</Poem>");
+            }
+        }
+    }
+}
---
+++ Weekly Homework/Code/Week2/CIS274_XML_Programming_Project/CIS274_XML_Programming_Project/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS274_XML_Programming_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CombinePoemsToXml();
+            ContinuePrompt();
+        }
+
+        public static void CombinePoemsToXml()
+        {
+            var sourcePath = @"H:\Google Drive\Projects\School\SPRING 2018\CIS274 XML Programming\Resources\Poems";
+            var targetPath = @"H:\Google Drive\Projects\School\SPRING 2018\CIS274 XML Programming\ScriptOutput\week2.xml";
+            var converter = new PoemConverter(sourcePath, targetPath);
+            string[] allFiles = Directory.GetFiles(sourcePath);
+            foreach (string file in allFiles)
+            {
+                var fileLines = converter.FileTextToLines(file);
+                converter.LinesToXml(fileLines);
+            }
+        }
+
+        /// <summary>
+        /// Prompts the user to continue by pressing enter.
+        /// </summary>
+        public static void ContinuePrompt()
+        {
+            Console.WriteLine("Press Enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
