7bf65c4995fde3b35a63d7c8bc84c4ed7d1bbb29
Zapafaz <Zapafaz@gmail.com>
2018-02-12 18:10:47 -0500

    Add basic comments

--- CIS274_XML_Programming_Project/CIS274_XML_Programming_Project/Program.cs
+++ CIS274_XML_Programming_Project/CIS274_XML_Programming_Project/Program.cs
@@ -10,18 +10,19 @@ namespace CIS274_XML_Programming_Project
 {
     class Program
     {
-        private XDocument poemSet;
-
         static void Main(string[] args)
         {
             CreatePoemDoc();
             ContinuePrompt();
         }
 
+        /// <summary>
+        /// Creates an XDocument with a set of poems from the poems folder and saves it to the script output folder.
+        /// </summary>
         public static void CreatePoemDoc()
         {
             var sourcePath = @"H:\Google Drive\Projects\School\SPRING 2018\CIS274 XML Programming\Resources\Poems";
-            var targetPath = @"H:\Google Drive\Projects\School\SPRING 2018\CIS274 XML Programming\ScriptOutput\week3.xml";
+            var targetPath = @"H:\Google Drive\Projects\School\SPRING 2018\CIS274 XML Programming\ScriptOutput\week4.xml";
             var doc = new XDocument();
             var converter = new PoemConverter(sourcePath);
             doc.Add(converter.ConvertFilesToPoemSet());
